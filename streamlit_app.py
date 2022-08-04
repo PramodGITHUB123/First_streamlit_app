fc3160d30048d196459ae6d792df305b75cd51e6
PramodGITHUB123 <110595917+PramodGITHUB123@users.noreply.github.com>
2022-08-04 23:05:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
-import streamlit
-streamlit.title('My Parents new Healthy dinner')
-streamlit.header('Breakfast menu')
-streamlit.text('omega 3,oatmeal')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
